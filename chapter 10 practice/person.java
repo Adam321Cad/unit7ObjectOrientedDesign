db1b84f4eec60c4cbd913f38deabf6d779a2d6b7
Adam321Cad <aratoad@gmail.com>
2015-02-10 11:26:46 -0600

    yuje
    sht

---
+++ chapter 10 practice/person.java
@@ -0,0 +1,35 @@
+
+
+/**
+ * Write a description of class person here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class person implements Comparable<person>
+{
+    String name;
+    
+    public person(String n)
+    {
+        name = n;
+    }
+    
+    public int compareTo(person other)
+    {
+        person otherperson = other;
+
+        if(this.name.compareTo(otherperson.name) >0)
+        {
+            return 1;
+        }else if(this.name.compareTo(otherperson.name) <0)
+        {
+            return 1;
+        }
+        else{
+            return 0;
+        }
+        
+    }
+
+}
