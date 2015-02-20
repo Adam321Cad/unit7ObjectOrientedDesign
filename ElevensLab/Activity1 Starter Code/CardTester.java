ec449b02e2e03ba7976c1b3f988d34c7f94125a9
Adam321Cad <aratoad@gmail.com>
2015-02-20 11:16:45 -0600

    work on this
    some time

--- ElevensLab/Activity1 Starter Code/CardTester.java
+++ ElevensLab/Activity1 Starter Code/CardTester.java
@@ -9,6 +9,21 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card c1 = new Card("king", "hearts", 13);
+		Card c2 = new Card("king", "hearts", 13);
+		Card c3 = new Card("ace", "clubs", 1);
+		Card c4 = new Card("king", "clubs", 13);
+		
+		System.out.println(c1.suit()+c1.rank()+c1.pointValue());
+		System.out.println(c2.suit()+c2.rank()+c2.pointValue());
+		System.out.println(c3.suit()+c3.rank()+c3.pointValue());
+		
+		System.out.println(c1.toString());
+		System.out.println(c2.toString());
+		System.out.println(c3.toString());
+		
+		System.out.println(c1.matches(c2));
+		System.out.println(c1.matches(c3));
+		System.out.println(c1.matches(c4));
 	}
 }
